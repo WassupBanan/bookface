5d5cbd14662fd677a134c8e76f4a4f3b7c5d706b
Banan <lim_en_yu_tianna@students.edu.sg>
2026-09-05 11:11:40 +0800

    Create StyledLink.js

---
+++ frontend/src/components/StyledLink.js
@@ -0,0 +1,17 @@
+import styled from
+  "@emotion/styled";
+
+import {
+  Link,
+} from "react-router-dom";
+
+export const StyledLink =
+  styled(Link)`
+    text-decoration: none;
+
+    color: inherit;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  `;
