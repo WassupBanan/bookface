b9415e2e041359f3a093cb1ac6e9354df225d526
Banan <lim_en_yu_tianna@students.edu.sg>
2026-09-05 11:03:53 +0800

    Create login.js

---
+++ frontend/src/pages/auth/login/login.js
@@ -0,0 +1,19 @@
+import {
+  Container,
+} from "@mui/material";
+
+import SignIn from "./SignIn";
+
+export default function Login() {
+  return (
+    <>
+      <Container
+        sx={{
+          height: "20px",
+        }}
+      />
+
+      <SignIn />
+    </>
+  );
+}
